2217c8122790b2c2261fe4a1e0d94ea776dd7694
Muhannad <muhannadalsrahen@gmail.com>
2025-08-22 17:30:03 +0300

    Add ChallengePageHeader component and update dependencies in package.json and bun.lock

--- src/routes/challenges/index.tsx
+++ src/routes/challenges/index.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ChallengePageHeader } from '@/services/challenges'
 
 export const Route = createFileRoute('/challenges/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/challenges/"!</div>
+  return (
+    <div className='bg-background'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-8xl mx-auto'>
+          <ChallengePageHeader />
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/services/challenges/components/ChallengePageHeader.tsx
@@ -0,0 +1,12 @@
+export function ChallengePageHeader() {
+  return (
+    <div className='flex flex-col gap-2 text-center'>
+      <h1 className='text-4xl font-bold text-primary'>Frontend Challenges</h1>
+
+      <p className='text-xl text-muted-foreground max-w-2xl mb-12 text-center mx-auto'>
+        Master frontend development through hands-on challenges designed to
+        sharpen your skills and build portfolio-worthy projects
+      </p>
+    </div>
+  )
+}
