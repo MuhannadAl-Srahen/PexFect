de2f0dacb238686a1a734a59fc406884b7be8f1d
Abdulrahman <abdkalnaser@gmail.com>
2025-10-10 17:31:09 +0300

    Auto-commit: index.ts updated

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,11 @@
 export { cn } from './utils'
 export { queryClient } from './queryClient'
 export { ensureProfileExists, getUserProfile, updateUserProfile } from './profileHelpers'
+export { 
+  isChallengeeSaved, 
+  saveChallenge, 
+  unsaveChallenge, 
+  toggleChallengeSave,
+  getSavedChallenges,
+  getSavedChallengeIds
+} from './savedChallenges'
